f65412b3b2d76e3f82975e536f46e545f376058d
Sariah Jackson <sarijah13@gmail.com>
2024-10-23 17:56:43 -0400

    contact links hover effect added

--- src/components/ProjectLinks.jsx
+++ src/components/ProjectLinks.jsx
@@ -6,17 +6,17 @@ import { LuExternalLink } from "react-icons/lu";
 export default function ProjectLinks(props) {
   return (
     <div className='flex gap-10'>
-                <div className="text-white">
+                <div className="text-coral">
                     <Link to={props.demoLink} className='flex items-center gap-3'>
                         <LuExternalLink className='h-6 w-6' />
-                        <p>View live</p>
+                        <p className='text-coral'>View live</p>
                     </Link>
                 </div>
                 
-                <div className="text-white">
+                <div className="text-coral">
                     <Link to={props.codeLink} className='flex items-center gap-3'>
                         <FaGithub className='h-6 w-6' />
-                        <p>Code</p>
+                        <p className='text-coral'>Code</p>
                     </Link>
                 </div>
             </div>
